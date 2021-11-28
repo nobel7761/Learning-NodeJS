e9c51e49fe14c7d262c6ed46be969355f936ca01
nobel7761 <habiburnobel@gmail.com>
2021-11-29 02:44:50 +0600

    file system module(asynchronous)

--- Built-in Node Module/app.js
+++ Built-in Node Module/app.js
@@ -1,9 +1,15 @@
 const fs = require('fs');
 
-let text = fs.readFileSync('./files/input.txt', 'utf-8');
-
-console.log(text);
-
-text = `Copied Text: ${text}`;
-
-fs.writeFileSync('./files/output.txt', text);
+fs.readFile('./files/input.txt', 'utf-8', (error, text) => {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log(text);
+        fs.writeFile('./files/output2.txt', text, 'utf-8', (error) => {
+            if (error) {
+                console.log(error);
+            }
+        });
+    }
+});
