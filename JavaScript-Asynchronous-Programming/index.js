461b5dab8f6dd4d6debb5b3087621c94035df50b
nobel7761 <habiburnobel@gmail.com>
2021-11-30 22:08:35 +0600

    Nested CallBack Function/ CallBack Hell

--- JavaScript-Asynchronous-Programming/index.js
+++ JavaScript-Asynchronous-Programming/index.js
@@ -2,13 +2,33 @@ console.log('Line 1');
 
 getStudent(2, (student) => {
     console.log(student);
+    getStudentCourses(student, (courses) => {
+        console.log(courses);
+        getStudentMarks(courses, (marks) => {
+            console.log(marks);
+        })
+    })
 })
 
 console.log('Line 2');
 
-function getStudent(id, callback) {
+function getStudent(id, cb) {
     setTimeout(() => {
-        console.log('Fetching Student Data...');
-        callback({ id: id, name: 'Nobel' });
+        console.log('Fetching Student from Database...');
+        cb({ id: id, name: 'Nobel' });
+    }, 3000);
+}
+
+function getStudentCourses(student, cb) {
+    setTimeout(() => {
+        console.log('Fetching Student Courses from Database...');
+        cb({ id: student.id, name: student.name, courses: ['JavaScript', 'Python'] })
+    }, 3000)
+}
+
+function getStudentMarks(courses, cb) {
+    setTimeout(() => {
+        console.log('Fetching Student Marks from Database...');
+        cb({ id: courses.id, name: courses.name, courses: courses.courses, marks: { [courses.courses[0]]: 80, [courses.courses[1]]: 90 } }); //object er property jodi variable aakare likhte hoy tokhn sheta [] bracket er vitore likhte hoy.
     }, 3000)
 }
